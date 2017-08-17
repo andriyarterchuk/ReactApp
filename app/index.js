5c3b2032d050385365b11b955481054f6e05b779
andriyarterchuk <andriyarterchuk@gmail.com>
2017-08-17 14:48:51 +0300

    Add compiled files

--- app/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import Main from './components/main';
-require('../dist/style.css');
-
-ReactDom.render(
-  <Main />, document.getElementById('root')	
-);
